a73f6ccfa3907874527d0b099b99769cf8770c5c
Diego Dubois <ddubois1780@gmail.com>
2025-03-20 02:02:02 +0100

    Change BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import App from './App.jsx'
 import "./assets/css/style.css";
 import "./assets/css/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
